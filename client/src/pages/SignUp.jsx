221bfe9c60279a2357870117b6d6eb491ff4f140
Neeraj Kumawat <neerajkumawat10200@gmail.com>
2024-08-12 00:50:58 +0530

    Completed Signup page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,37 @@
+import { Link } from "react-router-dom";
 const SignUp = () => {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center font-semibold text-3xl my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-slate-700 p-3 text-white rounded-lg hover:opacity-95 disabled:opacity-80">
+          SIGN UP
+        </button>
+      </form>
+      <div className="flex gap-3 my-4">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-700 font-semibold">Sign In</span>
+        </Link>
+      </div>
     </div>
   );
 };
